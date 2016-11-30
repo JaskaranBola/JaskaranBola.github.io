c286cd6014a3b2ff48cfd813081e45152f98dc50
JaskaranBola <jaskaranb@hotmail.com>
2016-11-30 12:37:47 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-
+//var input = $('input[name=userinput]').val();
+$('#userinput').keypress(function(e) {
+  var key = e.which;
+  if (key == 13) // the enter key code
+  {
+    alert("sds");
+  }
+});
