f3c4f4d29d8e75f874519311ac9bd656eb051284
JaskaranBola <jaskaranb@hotmail.com>
2016-11-30 21:13:03 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
-//var input = $('input[name=userinput]').val();
-$('#userinput').keypress(function(e) {
-  var key = e.which;
-  if (key == 13) // the enter key code
-  {
-    alert("sds");
-  }
+$(document).ready(function() {
+  var input = $('input[name=userinput]').val();
+  $('#userinput').keypress(function(e) {
+    var key = e.which;
+    if (key == 13) // the eenter key cod
+    {
+      alert("sss");
+      var input = $('input[name=userinput]').val();
+      if (input === 'north') {
+        $('.actions').text("You went" + input);
+      }
+    }
+  });
 });
