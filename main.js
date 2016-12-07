2a6d72413f06e925e2000c93813399b6a923fc7c
JaskaranBola <jaskaranb@hotmail.com>
2016-12-06 21:47:28 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#userinput').keypress(function(e) {
     var key = e.which;
-    if (key == 13) // Enter code starts here
+    if (key === 13) // Enter code starts here
     {
       var go = ['walk', 'go', 'run', 'crawl', 'jump'];
       var see = ['look', 'see', 'observe', 'scan', 'look around', 'scan area'];
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
       for (var i = 0; i < go.length; i++) {
         if (input === "back" && img.attr('src') !== "http://www.nikdaum.com/news/09shanghai1625.jpg") {
-          $('.story').html("You came back to the center of the apartment, now what? Maybe you should try <u>looking</u> around or going towards another <u>direction</u>.");
+          $('.story').html("You came back to the center of the apartment, now what? Maybe you should try <u>looking</u> around or going towards another <u>direction</u>");
           $('.actions').html("<span class=\"actionoutput\"><i>You went back to the room</span>");
           level = 1;
         }
@@ -38,26 +38,30 @@ $(document).ready(function() {
         img.fadeOut(1000, function() {
           img.attr("src", "http://www.nikdaum.com/news/09shanghai1625.jpg").fadeIn(1000);
         });
+        $('.itemsNotif').html(" ");
       } else if (level === 2 && img.attr('src') === "http://www.nikdaum.com/news/09shanghai1625.jpg") {
         img.fadeOut(1000, function() {
-          img.attr("src", "north.jpg").fadeIn(1000);
+          img.attr("src", "https://media.gettyimages.com/videos/graphic-fades-in-small-question-mark-moves-closer-to-screen-then-out-video-id1B02262_0008?s=640x640").fadeIn(1000);
         });
         $('.itemsNotif').html(" ");
+        $('.story').html("You go towards the north and see...-It's too dark to see. You cannot see much, maybe you can <u>use</u> something to brighten the room or you can go <u>back</u> and try another direction.");
       } else if (level === 3 && img.attr('src') === "http://www.nikdaum.com/news/09shanghai1625.jpg") {
         img.fadeOut(1000, function() {
           img.attr("src", "http://creativefan.com/important/cf/2012/09/dark-kitchen-cabinets/old-style-kitchen.jpg").fadeIn(1000);
         });
         $('.itemsNotif').html(" ");
+        $('.story').html("You go towards the east and see");
       } else if (level === 4 && img.attr('src') === "http://www.nikdaum.com/news/09shanghai1625.jpg") {
         img.fadeOut(1000, function() {
-          img.attr("src", "south").fadeIn(1000);
+          img.attr("src", "https://media.gettyimages.com/videos/graphic-fades-in-small-question-mark-moves-closer-to-screen-then-out-video-id1B02262_0008?s=640x640").fadeIn(1000);
         });
         $('.itemsNotif').html(" ");
       } else if (level === 5 && img.attr('src') === "http://www.nikdaum.com/news/09shanghai1625.jpg") {
         img.fadeOut(1000, function() {
-          img.attr("src", "https://i.imgur.com/WhWoXqZ.jpg").fadeIn(1000);
+          img.attr("src", "https://docs.unrealengine.com/latest/images/Resources/Showcases/RealisticRendering/NightSceneNoLights/RoomNightNoLights.jpg").fadeIn(1000);
         });
         $('.itemsNotif').html(" ");
+        $('.story').html("You go towards the west and see a living room. The first thing you notice is the faint light emitting from a tipped over lamp. The next thing you notice is the window; it appears you are above ground level. The window is completely sealed and the glass doesn't seem brittle. There was no way you are going to be able to leave from this way. Try <u>looking</u> around or go <u>back</u> to the center.");
       } //end of level change
       for (var j = 0; j < see.length; j++) {
         if (input === see[j]) {
@@ -65,11 +69,15 @@ $(document).ready(function() {
           if (img.attr('src') === "http://www.nikdaum.com/news/09shanghai1625.jpg") {
             $('.itemsNotif').html("<span class=\"foundItems\"><u>flashlight</u></span>");
           } else if (img.attr('src') === "http://creativefan.com/important/cf/2012/09/dark-kitchen-cabinets/old-style-kitchen.jpg") {
-            $('.itemsNotif').html("<span class=\"foundItems\"><u>A flashlight</u></span>");
+            $('.itemsNotif').html("<span class=\"foundItems\"><u> </u></span>");
+          } else if (img.attr('src') === "http://theblacksheeponline.com/wp-content/uploads/2016/03/powerout.png") {
+            $('.itemsNotif').html("<span class=\"foundItems\"><u>Piece of paper</u></span>");
           } else if (img.attr('src') === "https://i.imgur.com/WhWoXqZ.jpg") {
-            $('.itemsNotif').html("<span class=\"foundItems\"><u>A flashlight</u></span>");
-          } else if (img.attr('src') === "north.jpg") {
-            $('.itemsNotif').html("<span class=\"foundItems\"><u>key</u></span>");
+            $('.itemsNotif').html("<span class=\"foundItems\"><u>Key</u></span>");
+          } else if (img.attr('src') === "https://docs.unrealengine.com/latest/images/Resources/Showcases/RealisticRendering/NightSceneNoLights/RoomNightNoLights.jpg") {
+            $('.itemsNotif').html("<span class=\"foundItems\"><u> </u></span>");
+          } else if (img.attr('src') === "https://media.gettyimages.com/videos/graphic-fades-in-small-question-mark-moves-closer-to-screen-then-out-video-id1B02262_0008?s=640x640"); {
+            $('.itemsNotif').html("<span class=\"blind\">You cannot see anything! <span class=\"joke\">Try turning on the lights next time..</span></span>");
           }
         }
       } //end of see 
@@ -81,7 +89,7 @@ $(document).ready(function() {
             $('.items').append("<img class=\'icons\' src=\"https://cdn4.iconfinder.com/data/icons/proglyphs-miscellaneous/512/Flashlight-512.png\">");
           }
         }
-        if (img.attr('src') === "north.jpg") {
+        if (img.attr('src') === "https://i.imgur.com/WhWoXqZ.jpg") {
           if (input === pickup[k] + " " + "key") {
             inventory.push("key");
             $('.actions').html("<span class=\"actionoutput\"><i>You picked up a key</span>");
